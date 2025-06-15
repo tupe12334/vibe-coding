467bc330b5a275ddef090fa0641737ec0f2a48d4
tupe12334 <ofek.gabay.he@gmail.com>
2025-06-15 22:37:57 +0300

    refactor: remove commented-out project type function and associated documentation

--- builder/src/cli/main.ts
+++ builder/src/cli/main.ts
@@ -1,30 +1,7 @@
-/**
- * Checkbox list examples
- */
-
 import { writeFile } from "fs/promises";
 import { builder } from "../builder";
 import { getLanguage } from "./getLanguage";
 import { outputPath } from "./outputPath";
-
-// export async function projectType() {
-//   return inquirer.prompt([
-//     {
-//       type: "select",
-//       message: "What type of project are you building?",
-//       name: "projectType",
-//       choices: [
-//         { name: "Web Application", value: "web" },
-//         { name: "Mobile Application", value: "mobile" },
-//         { name: "Desktop Application", value: "desktop" },
-//         { name: "API Service", value: "backend" },
-//         { name: "Library/Package", value: "library" },
-//         new inquirer.Separator(" = Other = "),
-//         { name: "Other", value: "other" },
-//       ],
-//     },
-//   ]);
-// }
 
 export const main = async () => {
   const language = await getLanguage();
