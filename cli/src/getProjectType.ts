9f1dd880f615ec889c2964668cfbf4a596dec8a2
tupe12334 <ofek.gabay.he@gmail.com>
2025-06-16 15:10:57 +0300

    refactor: replace dynamic project type options with static ProjectTypes from builder

--- cli/src/getProjectType.ts
+++ cli/src/getProjectType.ts
@@ -1,16 +1,14 @@
 import { select } from "@inquirer/prompts";
+import { ProjectTypes } from "@vibe-builder/builder";
 import { readdir } from "fs/promises";
 import { basename, extname } from "path";
 
 export const getProjectType = async () => {
-  const options = await (
-    await readdir("./templates/project")
-  ).map((filename) => basename(filename, extname(filename)));
-
+ 
   return select({
     message: "What type of project are you working on?",
     default: "frontend",
-    choices: options.map((project) => ({
+    choices: ProjectTypes.map((project) => ({
       name: project,
       value: project.toLowerCase(),
     })),
