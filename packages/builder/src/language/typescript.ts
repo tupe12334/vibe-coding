cba7cd519ac767bb92c1b7e81763f261c7072d56
tupe12334 <ofek.gabay.he@gmail.com>
2025-06-21 00:39:41 +0300

    feat(rules): add guideline to avoid using plain TypeScript enums

--- packages/builder/src/language/typescript.ts
+++ packages/builder/src/language/typescript.ts
@@ -1,5 +1,6 @@
 export const typescriptRules = [
-  "Use Interfaces over Types unless needed."
+  "Use Interfaces over Types unless needed.",
+  "Don't use plain TypeScript enums",
 ] as const;
 
 export type TypescriptRule = (typeof typescriptRules)[number];
