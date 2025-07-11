79fdcc33465a7a932fb362f8a2e267e08660ea31
tupe12334 <ofek.gabay.he@gmail.com>
2025-07-11 16:02:06 +0300

    feat(rules): add guideline to prefer using composite prop types over primitive types

--- packages/builder/src/general/rules.ts
+++ packages/builder/src/general/rules.ts
@@ -16,6 +16,7 @@ export const generalRules = [
   "Always use a package to manage and validate environment variables.",
   "If you change or create a test in you work, always run the test and make sure it passes.",
   "Try to have less then 50 lines of code in a single file.",
+  "Prefer using composite prop types over primitive types.",
 ] as const;
 
 export type GeneralRule = (typeof generalRules)[number];
