b747f6adcd97f974931b0969ddeb80065cf012b2
tupe12334 <ofek.gabay.he@gmail.com>
2025-08-01 13:48:29 +0300

    fix: add additional React rule for better hook usage guidance

--- packages/builder/src/framework/react.ts
+++ packages/builder/src/framework/react.ts
@@ -1,5 +1,6 @@
 export const reactRules = [
-  "Use key in React lists to help React identify which items have changed, are added, or removed."
+  "Use key in React lists to help React identify which items have changed, are added, or removed.",
+  "Don't put logic into hooks, hold them in functions and just call them inside the needed hooks.",
 ] as const;
 
 export type ReactRule = (typeof reactRules)[number];
